ea51442950224b81dcd65b7bcc7215d9b8bb0b40
Johnny Winn and Travis Anderson <dev+nurugger07+syntaxritual@hashrocket.com>
2013-08-02 15:28:15 -0400

    WIP

--- test/features/step_definitions/user_steps.rb
+++ test/features/step_definitions/user_steps.rb
@@ -23,9 +23,26 @@ Then "I see that user info in the session" do
   end
 end
 
+Then "I am on the view profile page" do
+  current_url.should eq("/user/1")
+end
+
 Given "I am signed in" do
   step %{there is a user "johnny@example.com"}
   step %{I am on the home page}
   step %{I follow "Sign In"}
   step %{I submit valid credentials}
+end
+
+When "I update the user profile information" do
+  fill_in :full_name, with: "Robert Terwilliger"
+  fill_in :twitter, with: "_sideshow_bob"
+  fill_in :github, with: "sideshow-bob"
+  fill_in :email, with: "sideshow_bob@example.com"
+  fill_in :password, with: USER[:password]
+  fill_in :confirm, with: USER[:password]
+
+  fill_in :bio, with: "Robert Underdunk Terwilliger, better known by his stage name Sideshow Bob, is a recurring character in the animated television series The Simpsons. He is voiced by Kelsey Grammer and first appeared briefly in the episode \"The Telltale Head\"."
+
+  click_button "Save"
 end
--- web/routers/users_router.ex
+++ web/routers/users_router.ex
@@ -2,9 +2,13 @@ defmodule UsersRouter do
   use Dynamo.Router
 
   get "/:user_id" do
-    render_user(conn, UserQueries.find_by_id(user_id))
+    render_user(conn, "user/show.html", UserQueries.find_by_id(user_id))
   end
 
-  def render_user(conn, user) when user == nil, do: render(conn, "404.html", layout: "secondary")
-  def render_user(conn, user), do: render(conn, "user/show.html", user: user)
+  get "/:user_id/edit" do
+    render_user(conn, "user/edit.html", UserQueries.find_by_id(user_id))
+  end
+
+  def render_user(conn, user, _page) when user == nil, do: render(conn, "404.html", layout: "secondary")
+  def render_user(conn, user, page), do: render(conn, page, user: user)
 end
